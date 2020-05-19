6a690ea146bd21fe2dfedd2d2a900090bd1f6c83
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-05-20 00:13:30 +0100

    Router set up with one navigation link. Tested by adding /home instead of just / in link and route. Switch added to switch component on click of button. Step 3.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,25 @@
 import React from 'react';
 // npm i react-router-dom
 import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import Home from './Home';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>A navbar will go here shortly.</h1>
-        </div>
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                </ul>
+
+                <Switch>
+                    <Route exact path='/'>
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
