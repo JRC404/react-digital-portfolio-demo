ef8dca733b1be6f07e2a660badc0285b2aed0642
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-05-20 00:15:03 +0100

    About component added and imported. Included in router and switch. Step 4.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 // npm i react-router-dom
 import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
 
 const Navbar = () => {
     return (
@@ -11,11 +12,17 @@ const Navbar = () => {
                     <li>
                         <Link to='/'>Home</Link>
                     </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
                 </ul>
 
                 <Switch>
                     <Route exact path='/'>
                         <Home />
+                    </Route>
+                    <Route path='/about'>
+                        <About />
                     </Route>
                 </Switch>
             </div>
