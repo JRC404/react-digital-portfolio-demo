0dacbcb7ac28c2ae90ba9038f2e7cbb19ad672b3
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-05-20 00:09:01 +0100

    Component for navbar created and imported. That is step 1.

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>A navbar will go here shortly.</h1>
+        </div>
+    )
+}
+
+export default Navbar;
