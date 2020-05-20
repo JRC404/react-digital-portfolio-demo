62fa2654f051cb47f35d8a5cb8baee12165540c1
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-05-20 17:48:16 +0100

    Router pulled out of navbar and added to App.js for stronger structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Employment from './Components/Employment';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/employment'>
+            <Employment />
+          </Route>
+          <Route path='/projects'>
+            <Projects />
+          </Route>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,54 +1,30 @@
 import React from 'react';
 // npm i react-router-dom
-import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import Employment from './Employment';
-import Projects from './Projects';
-import Contact from './Contact';
-
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <Router>
-            <div>
-                <ul>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/about'>About</Link>
-                    </li>
-                    <li>
-                        <Link to='/employment'>Employment</Link>
-                    </li>
-                    <li>
-                        <Link to='/projects'>Projects</Link>
-                    </li>
-                    <li>
-                        <Link to='/contact'>Contact</Link>
-                    </li>
-                </ul>
+        <div>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/employment'>Employment</Link>
+                </li>
+                <li>
+                    <Link to='/projects'>Projects</Link>
+                </li>
+                <li>
+                    <Link to='/contact'>Contact</Link>
+                </li>
+            </ul>
 
-                <Switch>
-                    <Route exact path='/'>
-                        <Home />
-                    </Route>
-                    <Route path='/about'>
-                        <About />
-                    </Route>
-                    <Route path='/employment'>
-                        <Employment />
-                    </Route>
-                    <Route path='/projects'>
-                        <Projects />
-                    </Route>
-                    <Route path='/contact'>
-                        <Contact />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
+
+        </div>
     )
 }
 
