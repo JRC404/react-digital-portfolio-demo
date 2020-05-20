ec2a019a4c8dd1d35dc38e4a204e36a1a5f8178e
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-05-20 01:00:18 +0100

    Contact form added. Readme to follow for this.

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,6 +4,13 @@ const Contact = () => {
     return (
         <div>
             <h1>Hello, Contact.</h1>
+            <form>
+                <label>
+                    Name:
+                <input type="text" name="name" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
